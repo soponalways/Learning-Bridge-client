bfef4633f9b6fae5789f71be987cc44bade23f40
Sopon islam <soponalways@gmail.com>
2026-04-21 01:31:31 +0600

    Better auth client instance create and t3oss env setup for type saftey on env

---
+++ lib/auth-client.ts
@@ -0,0 +1,6 @@
+import { env } from "@/app/env"
+import { createAuthClient } from "better-auth/react"
+export const authClient = createAuthClient({
+    /** The base URL of the server (optional if you're using the same domain) */
+    baseURL: env.SERVER_URL
+})
